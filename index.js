ca28f395cc1e08b78e52426b49bc87f502a4179c
Kevin Cui <krazycavin@gmail.com>
2018-09-29 15:34:00 +0200

    Fix lint

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const puppeteer = require("puppeteer");
-const atob = require("atob");
+const puppeteer = require('puppeteer')
+const atob = require('atob')
 
-var chrome = "/usr/bin/chromium";
-var isheadless = true;
+var chrome = '/usr/bin/chromium'
+var isheadless = true
 
 puppeteer
   .launch({ executablePath: chrome, headless: isheadless })
   .then(async browser => {
-    const page = await browser.newPage();
+    const page = await browser.newPage()
 
-    await page.goto("https://ludwig.guru/s/ludwig");
-    let cookie = await page.cookies();
+    await page.goto('https://ludwig.guru/s/ludwig')
+    let cookie = await page.cookies()
 
-    let jwt = cookie.filter(c => c["name"] == "_ljwt");
-    let json = JSON.parse(atob(jwt[0]["value"]));
+    let jwt = cookie.filter(c => c['name'] === '_ljwt')
+    let json = JSON.parse(atob(jwt[0]['value']))
 
-    console.log(json["token"]);
-    await browser.close();
-  });
+    console.log(json['token'])
+    await browser.close()
+  })
