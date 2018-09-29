2362642eae324d79c13cdfb0f05fefd3c76d65ba
Kevin Cui <krazycavin@gmail.com>
2018-09-29 15:13:29 +0200

    Use cookie to fetch token

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const atob = require("atob");
 
-var chrome="/usr/bin/chromium"
-var isheadless=true
+var chrome = "/usr/bin/chromium";
+var isheadless = true;
 
-puppeteer.launch({executablePath: chrome, headless: isheadless}).then(async browser => {
-  const page = await browser.newPage();
+puppeteer
+  .launch({ executablePath: chrome, headless: isheadless })
+  .then(async browser => {
+    const page = await browser.newPage();
 
-  await page.goto('https://ludwig.guru');
-  const json = await page.evaluate(async () => {
-    id = localStorage.getItem('ludwig.lgid').replace(/['"]+/g, '');
-    return(JSON.parse(window.atob(id)))
-  })
+    await page.goto("https://ludwig.guru/s/ludwig");
+    let cookie = await page.cookies();
 
-  console.log(json['token'])
-  await browser.close();
-});
+    let jwt = cookie.filter(c => c["name"] == "_ljwt");
+    let json = JSON.parse(atob(jwt[0]["value"]));
+
+    console.log(json["token"]);
+    await browser.close();
+  });
